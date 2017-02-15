9844624d2d8f778cbf02de5c366e5d44427badad
Medhat Dawoud <medhatdawoud@gmail.com>
2017-02-15 20:04:52 +0200

    Added bootstrap instead of material lite - add a header - remove unused codes - enhance the header design

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -11,29 +11,28 @@
 import React from 'react';
 import Navigation from '../Navigation';
 import Link from '../../common/Link';
+import { Nav, Navbar, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 import s from './Header.css';
 
 class Header extends React.Component {
 
-  componentDidMount() {
-    window.componentHandler.upgradeElement(this.root);
-  }
-
-  componentWillUnmount() {
-    window.componentHandler.downgradeElements(this.root);
-  }
-
   render() {
     return (
-      <header className={`mdl-layout__header ${s.header}`} ref={node => (this.root = node)}>
-        <div className={`mdl-layout__header-row ${s.row}`}>
-          <Link className={`mdl-layout-title ${s.title}`} to="/">
-            React Static Boilerplate
-          </Link>
-          <div className="mdl-layout-spacer" />
-          <Navigation />
-        </div>
-      </header>
+      <div className={s.root}>
+        <Navbar className={s.navbar} inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">PreSales Showcase</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">Login</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -17,23 +17,15 @@ import s from './Layout.css';
 class Layout extends React.Component {
 
   static propTypes = {
-    className: PropTypes.string,
+    
   };
-
-  componentDidMount() {
-    window.componentHandler.upgradeElement(this.root);
-  }
-
-  componentWillUnmount() {
-    window.componentHandler.downgradeElements(this.root);
-  }
 
   render() {
     return (
-      <div className="mdl-layout mdl-js-layout" ref={node => (this.root = node)}>
-        <div className="mdl-layout__inner-container">
+      <div ref={node => (this.root = node)}>
+        <div className="container-fluid">
           <Header />
-          <main className="mdl-layout__content">
+          <main className="container">
             <div {...this.props} className={cx(s.content, this.props.className)} />
             <Footer />
           </main>
