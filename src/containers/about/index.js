649eb8223720e6dac30a1ce8aafdeea16a8042f1
Medhat Dawoud <medhatdawoud@gmail.com>
2017-02-15 15:10:06 +0200

    Delete unused files

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -11,7 +11,6 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import s from './styles.css';
-import { title, html } from './index.md';
 
 class AboutPage extends React.Component {
 
@@ -22,11 +21,7 @@ class AboutPage extends React.Component {
   render() {
     return (
       <Layout className={s.content}>
-        <h1>{title}</h1>
-        <div
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+        
       </Layout>
     );
   }
